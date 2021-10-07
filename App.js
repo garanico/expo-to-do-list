bcee1a15fc5de1ce1f666c345cdcf6898862c2ae
Grace Aranico <grace.aranico@gmail.com>
2021-10-07 08:57:03 -0700

    added duplicate item check

--- App.js
+++ App.js
@@ -1,60 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  //items is the name of the array that holds  all of the to-do list tasks
+  const [items, setItems] = useState([
+    //{ todo: "first item" },
+    //{ todo: "second item" },,
+  ]);
 
+  const [newItemText, setNewItemText] = useState("");
 
-//items is the name of the array that holds  all of the to-do list tasks
-const [items, setItems] = useState([
-  //{todo: 'first item'},
-  //{todo: 'second item'},
-]);
+  const generateList = items.map((item, index) => (
+    <View key={index} style={styles.listItemContainer}>
+      <Text>{item.todo}</Text>
+    </View>
+  ));
 
-const [newItemText, setNewItemText] = useState('');
+  //it's changing variable text to the variable newItemText.
+  const onChangeText = (text) => {
+    newItemText = text;
+  };
+  const checkIfSame = () => {
+    for (let i = 0; i < items.length; i++) {
+      if (
+        items[i].todo.toLocaleLowerCase() === newItemText.toLocaleLowerCase()
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+  const checkIfValid = () => {
+    if (newItemText.length > 0 && isNaN(newItemText)) {
+      console.log(newItemText);
+      if (checkIfSame()) {
+        console.log(newItemText + " already exists.");
+      } else {
+        addToList();
+      }
+    } else {
+      console.log("Incorrect input");
+    }
+  };
 
-const generateList = items.map((item, index) => (
-  <View key={index} style={styles.listItemContainer}>
-    <Text>{item.todo}</Text>
-  </View>
-
-));
-
-
-//it's changing variable text to the variable newItemText.
-const onChangeText = (text) => {
-  newItemText = text;
-}
-
-const addToList = () => {
-  console.log({todo:newItemText});
-  console.log([...items, {todo: newItemText}]);
-  setItems([...items, {todo: newItemText}])
-}
-
-
+  const addToList = () => {
+    //console.log({todo:newItemText});
+    //console.log([...items, {todo: newItemText}]);
+    setItems([...items, { todo: newItemText }]);
+    setNewItemText("");
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.titleText}>To Do List</Text>
-      <TextInput 
-      placeholder="Add a task..." 
-      style={styles.textInputField} 
-      onChangeText={text => setNewItemText(text)} 
-      value = {newItemText}
+      <TextInput
+        placeholder="Add a task..."
+        style={styles.textInputField}
+        onChangeText={(text) => setNewItemText(text)}
+        value={newItemText}
       />
-      <Button 
-      title="Add to List" 
-      onPress={addToList}
-      />
-      <Button 
-      title="Clear" 
-      onPress={() => setNewItemText('')}
-      />
-      <ScrollView style={styles.scrollView}>
-        {generateList}
-      </ScrollView>
-        
+      <Button title="Add to List" onPress={checkIfValid} />
+      <Button title="Clear" onPress={() => setNewItemText("")} />
+      <ScrollView style={styles.scrollView}>{generateList}</ScrollView>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -63,32 +80,32 @@ const addToList = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
   titleText: {
-    fontSize: '20px',
+    fontSize: "20px",
   },
   textInputField: {
-    display: 'inline',
-    border: 'solid 1px #000',
+    display: "inline",
+    border: "solid 1px #000",
     borderRadius: 5,
-    width: '60%',
-    height: '2em',
-    lineHeight: '1em',
-    padding: '0.5em',
-    margin: '1em',
+    width: "60%",
+    height: "2em",
+    lineHeight: "1em",
+    padding: "0.5em",
+    margin: "1em",
   },
   scrollView: {
-    width: '100%'
+    width: "100%",
   },
   listItemContainer: {
-    alignItems: 'flex-start',
-    width: '100%',
+    alignItems: "flex-start",
+    width: "100%",
     paddingTop: 15,
     paddingBottom: 15,
     paddingRight: 30,
     paddingLeft: 30,
-  }
+  },
 });
